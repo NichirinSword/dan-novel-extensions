041b31e9c6a2f4dd43b6a127864332231db6433d
NichirinSword <98078687+NichirinSword@users.noreply.github.com>
2026-08-03 10:37:18 +0600

    Add files via upload

--- extensions-lib-stub/src/main/kotlin/eu/kanade/tachiyomi/network/Requests.kt
+++ extensions-lib-stub/src/main/kotlin/eu/kanade/tachiyomi/network/Requests.kt
@@ -1,1 +1,7 @@
+package eu.kanade.tachiyomi.network
 
+import okhttp3.Headers
+import okhttp3.Request
+
+fun GET(url: String, headers: Headers = Headers.Builder().build()): Request =
+    Request.Builder().url(url).headers(headers).build()
